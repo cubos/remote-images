ce116e5b620cf9a57adcb43c53e9c7e0f0093cb1
Guilherme Bernal <guilherme@cubos.io>
2021-07-25 20:55:48 -0400

    feat: add Google Cloud SDK

--- ubuntu-latest/update-readme.sh
+++ ubuntu-latest/update-readme.sh
@@ -11,6 +11,8 @@ TARGET=$(echo -n "<!-- BEGIN GENERATED SECTION: $IMAGE -->
 | Docker | $(docker -v | cut -d' ' -f 3 | sed 's/.$//') |
 | Git | $(git --version | cut -d' ' -f 3) |
 | Go | $(go version | cut -d' ' -f 3 | cut -c 3-) |
+| Google Cloud SDK | $(gcloud --version | head -n 1 | cut -d' ' -f 4) |
+| kubectl | $(kubectl version --client -o json | jq -r ".clientVersion.gitVersion" | cut -c 2-) |
 | Node.js | $(nvm use 12 >/dev/null && node -v | cut -c 2-)<br>$(nvm use 14 >/dev/null && node -v | cut -c 2-) (default)<br>$(nvm use 16 >/dev/null && node -v | cut -c 2-) |
 | Terraform | $(terraform -v | head -n1 | cut -d'v' -f 2) |
 
