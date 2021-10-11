bfaee5b7e92445471a64a817877a581451f49c6c
Guilherme Bernal <guilherme@cubos.io>
2021-10-11 15:57:53 -0300

    Add Clang and LLVM (#7)

--- ubuntu/first-run.sh
+++ ubuntu/first-run.sh
@@ -1,3 +1,4 @@
 # install docker-compose
+sudo mkdir -p $HOME/.docker/cli-plugins/
 sudo mv /opt/docker-compose $HOME/.docker/cli-plugins/docker-compose
-sudo chown $USER:$USER $HOME/.docker/cli-plugins/docker-compose
+sudo chown -R $USER:$USER $HOME/.docker
